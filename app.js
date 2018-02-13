f721920dd9a76d06e9b1c1f5e8ffb69c7371befc
Michael Sklepowich <michaelsklepowich@Michaels-MacBook-Pro-2.local>
2018-02-12 21:42:15 -0800

    question 3 test passed

--- app.js
+++ app.js
@@ -55,11 +55,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var addition = sum(a, sum(b, c)[0])[0];
+  var multiplication = multiply(a, multiply(b, c)[0])[0];
+  var messageArray = [addition, multiplication, a + ' and ' + b + ' and ' + c + ' sum to ' + addition + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplication + '.'];
+  return messageArray;
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
